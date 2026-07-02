758b05ecd135554a57b765cb8cc30265be6c71fd
Md Danish Alam <rankersacademy0@gmail.com>
2026-07-02 08:26:51 +0530

    Adding some projects live link

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,5 +1,28 @@
 const projectsData = [
   {
+    title: 'CodeQuick',
+    description: 'CodeQuick is a browser-based coding platform that lets users write, compile, and run programs in C, C++, Java, and Python directly online. It provides a simple and interactive interface, removing the need for local compilers and making coding practice fast and accessible.',
+     technologies: ['React', 'Express.js', 'Judge0 API', 'Tailwind CSS'],
+      githubLink: 'https://github.com/Mddanishalam2/Codequick',
+      liveLink: 'https://codequick-v2-rhha.onrender.com/',
+  },
+  {
+    title: 'BSEB-Pathsala',
+    description: 'BSEB Pathshala is an educational web platform that helps students access organized study notes, important PDFs, and learning resources in one place. It provides subject-wise materials, easy navigation, and quick downloads, making exam preparation simple, accessible, and student-friendly for BSEB learners across different classes.',
+     technologies: ['React', 'TypeScript', 'Tailwind CSS','Firebase Storage','HTML5','CSS'],
+      githubLink: 'https://github.com/Mddanishalam2/BSEB-Pathsala',
+      liveLink: 'https://bsebpathsala.vercel.app/',
+  },
+   
+  {
+    title: 'Mausam-Finder',
+    description: 'A weather app built with HTML, CSS, and JavaScript that lets users check real-time weather reports of any city using a weather API.',
+     technologies: ['HTML','CSS','JavaScript','Weather API'],
+     githubLink: 'https://github.com/Mddanishalam2/Mausam-Finder',
+     liveLink: 'https://mddanishalam2.github.io/Mausam-Finder/',
+  },
+  
+  {
     title: 'Where is my Bus',
      description: 'Where Is My Bus is a real-time bus tracking system built using GPS 808, Node.js, and React with Leaflet.js. It allows users to track local buses live on a map, view routes, and get accurate arrival timings for a smoother travel experience.',
     technologies: ['React', 'Tailwind CSS', 'Node.js', 'Leaflet.js'],
@@ -13,27 +36,7 @@ const projectsData = [
      githubLink: '#',
     liveLink: '#',
   },
-  {
-    title: 'BSEB-Pathsala',
-    description: 'BSEB Pathshala is an educational web platform that helps students access organized study notes, important PDFs, and learning resources in one place. It provides subject-wise materials, easy navigation, and quick downloads, making exam preparation simple, accessible, and student-friendly for BSEB learners across different classes.',
-     technologies: ['React', 'TypeScript', 'Tailwind CSS','Firebase Storage','HTML5','CSS'],
-      githubLink: 'https://github.com/Mddanishalam2/BSEB-Pathsala',
-      liveLink: 'https://bsebpathsala.vercel.app/',
-  },
-  {
-    title: 'CodeQuick',
-    description: 'CodeQuick is a browser-based coding platform that lets users write, compile, and run programs in C, C++, Java, and Python directly online. It provides a simple and interactive interface, removing the need for local compilers and making coding practice fast and accessible.',
-     technologies: ['React', 'Express.js', 'Judge0 API', 'Tailwind CSS'],
-      githubLink: 'https://github.com/Mddanishalam2/Codequick',
-      liveLink: 'https://codequick-v2-rhha.onrender.com/',
-  },
-  {
-    title: 'Mausam-Finder',
-    description: 'A weather app built with HTML, CSS, and JavaScript that lets users check real-time weather reports of any city using a weather API.',
-     technologies: ['HTML','CSS','JavaScript','Weather API'],
-     githubLink: 'https://github.com/Mddanishalam2/Mausam-Finder',
-     liveLink: 'https://mddanishalam2.github.io/Mausam-Finder/',
-  },
+ 
 ];
 
 export default projectsData;
